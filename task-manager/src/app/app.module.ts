c1bf1b9f78b141e6e7f4bfea1d4cc6cc3a6fefa2
Edoardo Oranger <edo.oranger@gmail.com>
2024-11-03 16:58:35 +0100

    New dependency
    Add Angular Material dependency

--- task-manager/src/app/app.module.ts
+++ task-manager/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
       declarations: [AppComponent],
-      imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+      imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, BrowserAnimationsModule],
       providers: [],
       bootstrap: [AppComponent],
 })
