9d1f8fb2c05838b3df152e5d50ab837ae8dd3d73
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-25 12:48:37 +0100

    Updated Component
    Completed partial implementation of header

--- task-manager/src/app/header/header.component.ts
+++ task-manager/src/app/header/header.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChartBar, faDoorOpen, faHome, faList } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
       standalone: true,
       selector: 'tm-header',
       templateUrl: './header.component.html',
       styleUrls: ['./header.component.css'],
-      imports: [CommonModule],
+      imports: [CommonModule, FontAwesomeModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+      public get homeIcon() {
+            return faHome;
+      }
+
+      public get projectIcon() {
+            return faList;
+      }
+
+      public get statisticsIcon() {
+            return faChartBar;
+      }
+
+      public get logoutIcon() {
+            return faDoorOpen;
+      }
+
+}
--- task-manager/src/app/layout/layout.component.ts
+++ task-manager/src/app/layout/layout.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { SidebarLeftComponent } from '../sidebars';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
       standalone: true,
       selector: 'tm-layout',
       templateUrl: './layout.component.html',
       styleUrls: ['./layout.component.css'],
-      imports: [RouterOutlet],
+      imports: [RouterOutlet, CommonModule, HeaderComponent, SidebarLeftComponent, FooterComponent],
 })
 export class LayoutComponent {}
