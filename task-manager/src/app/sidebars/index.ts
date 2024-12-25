78ce1776cb19ebf7b6aeb6c3374373ce9ef80257
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-25 12:18:22 +0100

    Implemented Component
    Created component for left sidebar

---
+++ task-manager/src/app/sidebars/index.ts
@@ -0,0 +1,1 @@
+export { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
---
+++ task-manager/src/app/sidebars/sidebar-left/sidebar-left.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+      standalone: true,
+      selector: 'tm-sidebar-left',
+      templateUrl: './sidebar-left.component.html',
+      styleUrls: ['./sidebar-left.component.css'],
+      imports: [CommonModule],
+})
+export class SidebarLeftComponent {}
