8e47653816ae8d7b373bf9baacba30d76f41610b
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-26 11:25:25 +0100

    Completed header
    Completed navbar with responsive layout

--- task-manager/src/app/header/header.component.ts
+++ task-manager/src/app/header/header.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChartBar, faDoorOpen, faHome, faList } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
 
 @Component({
       standalone: true,
       selector: 'tm-header',
       templateUrl: './header.component.html',
       styleUrls: ['./header.component.css'],
-      imports: [CommonModule, FontAwesomeModule],
+      imports: [CommonModule, FontAwesomeModule, RouterLink],
 })
 export class HeaderComponent {
+
+      private activeElement: boolean;
+
+      @ViewChild('hamburger')
+      public readonly hamburgerMenu!: HTMLUListElement;
+
+      public constructor() {
+            this.activeElement = false;
+      }
+
+      public get active() {
+            return this.activeElement;
+      }
 
       public get homeIcon() {
             return faHome;
@@ -27,6 +41,14 @@ export class HeaderComponent {
 
       public get logoutIcon() {
             return faDoorOpen;
+      }
+
+      public get appIcon() {
+            return this.projectIcon;
+      }
+
+      public onToggleHamburgerMenu() {
+            this.activeElement = !this.activeElement;
       }
 
 }
