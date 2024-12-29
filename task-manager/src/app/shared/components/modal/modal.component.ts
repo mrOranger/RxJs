3b4a81a55241a17b3b1da585850b4270bf99652d
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-29 17:11:16 +0100

    Created Component
    Created component for Modal

---
+++ task-manager/src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+      standalone: true,
+      selector: 'tm-modal',
+      imports: [CommonModule],
+      templateUrl: './modal.component.html',
+      styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent {}
