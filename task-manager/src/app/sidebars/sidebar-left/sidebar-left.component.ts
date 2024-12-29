5ec87611d45c0e073359ed0b0224dba2cc6081b4
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-29 17:06:57 +0100

    Updated Component
    Created button to make a new task

--- task-manager/src/app/sidebars/sidebar-left/sidebar-left.component.ts
+++ task-manager/src/app/sidebars/sidebar-left/sidebar-left.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
       standalone: true,
       selector: 'tm-sidebar-left',
-      templateUrl: './sidebar-left.component.html',
+      imports: [CommonModule, FontAwesomeModule],
       styleUrls: ['./sidebar-left.component.css'],
-      imports: [CommonModule],
+      templateUrl: './sidebar-left.component.html',
+      changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarLeftComponent {}
+export class SidebarLeftComponent {
+
+      public onNewTask() {
+            console.log('onNewTask');
+      }
+
+      public get newTaskIcon() {
+            return faPlusCircle;
+      }
+
+}
