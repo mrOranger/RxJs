36f1a9b0c679138d4ce9e4d7119d7086042030b4
Edoardo Oranger <edo.oranger@gmail.com>
2024-12-31 14:58:53 +0100

    Update configuration
    Add animations providers

--- task-manager/src/app/app.config.ts
+++ task-manager/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
       providers: [
+            provideAnimations(),
             provideRouter(
                   routes,
                   withComponentInputBinding(),
