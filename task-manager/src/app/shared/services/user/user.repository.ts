b466df9827a4578669d6599dc4d837ada846dfa0
Edoardo Oranger <edo.oranger@gmail.com>
2025-01-02 12:37:20 +0100

    Updated Repository
    Fix missing method implementation

--- task-manager/src/app/shared/services/user/user.repository.ts
+++ task-manager/src/app/shared/services/user/user.repository.ts
@@ -6,5 +6,4 @@ import { CrudRepository } from '../crud-repository.interface';
 export interface UserRepository extends CrudRepository<User, string, 'id' | 'createdAt' | 'updatedAt'> {
       findByEmail(email: string): Observable<User>;
       findByEmailAndPassword(email: string, password: string): Observable<User>;
-      attach(userId: string, taskId: string): Observable<User>    ;
 }
