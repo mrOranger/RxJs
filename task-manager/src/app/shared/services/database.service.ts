32b9a5400a3dc5f511827403b9dd922bcda8f76c
Edoardo Oranger <edo.oranger@gmail.com>
2025-01-02 12:14:47 +0100

    Updated Service
    Add new collection for Task-User Table.

--- task-manager/src/app/shared/services/database.service.ts
+++ task-manager/src/app/shared/services/database.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 
 import Dexie, { EntityTable } from 'dexie';
 
-import { TaskEntity, UserEntity } from '../models';
+import { TaskEntity, TaskUserEntity, UserEntity } from '../models';
 
 @Injectable()
 export class DatabaseService extends Dexie {
       public readonly users!: EntityTable<UserEntity, 'id'>;
       public readonly tasks!: EntityTable<TaskEntity, 'id'>;
+      public readonly taskUser!: EntityTable<TaskUserEntity, 'id'>;
 
       public constructor() {
             super('TaskManagerDB');
@@ -15,10 +16,12 @@ export class DatabaseService extends Dexie {
             this.version(1).stores({
                   users: 'id, firstName, lastName, &email, [email+password], createdAt, updatedAt',
                   tasks: 'id, title, description, assignedTo, createdAt, updatedAt',
+                  taskUser: 'id, userId, taskId, createdAt, updatedAt',
             });
 
             this.users.mapToClass(UserEntity);
             this.tasks.mapToClass(TaskEntity);
+            this.taskUser.mapToClass(TaskUserEntity);
 
       }
 }
