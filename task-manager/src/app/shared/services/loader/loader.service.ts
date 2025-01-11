85fbd01afd828417354ca96660d38793c92c6d96
Edoardo Oranger <edo.oranger@gmail.com>
2025-01-11 17:31:25 +0100

    Fix Service
    Add timout for after change errors

--- task-manager/src/app/shared/services/loader/loader.service.ts
+++ task-manager/src/app/shared/services/loader/loader.service.ts
@@ -1,10 +1,8 @@
 import { ApplicationRef, ComponentRef, inject, Injectable, ViewContainerRef } from '@angular/core';
 import { LoaderComponent } from '../../components';
 
-
 @Injectable({ providedIn: 'root' })
 export class LoaderService {
-
       private componentRef: ComponentRef<LoaderComponent> | null;
       private readonly applicationRef: ApplicationRef;
 
@@ -14,17 +12,23 @@ export class LoaderService {
       }
 
       public start(text?: string): void {
-            const container = this.applicationRef.components[0].injector.get(ViewContainerRef);
-            const loaderComponentRef = container.createComponent(LoaderComponent);
-            loaderComponentRef.instance.label = text;
-            this.componentRef = loaderComponentRef;
+            const timeoutRef = setTimeout(() => {
+                  const container = this.applicationRef.components[0].injector.get(ViewContainerRef);
+                  const loaderComponentRef = container.createComponent(LoaderComponent);
+                  loaderComponentRef.instance.label = text;
+                  this.componentRef = loaderComponentRef;
+                  clearTimeout(timeoutRef);
+            }, 100);
       }
 
       public stop(): void {
-            if (this.componentRef !== null) {
-                  this.componentRef?.destroy();
-                  this.componentRef = null;
-            }
+            const timeoutRef = setTimeout(() => {
+                  if (this.componentRef !== null) {
+                        this.componentRef?.destroy();
+                        this.componentRef = null;
+                  }
+                  clearTimeout(timeoutRef);
+            }, 100);
       }
 
       public startAndStop(fn: () => void, message?: string) {
