bb795b16056e361693b12d92508bfbea6fa50eda
Edoardo Oranger <edo.oranger@gmail.com>
2025-01-11 17:31:07 +0100

    Add new Tokens
    Add new Dependenjy Injection Tokens for Repositories

--- task-manager/src/app/injection-tokens.ts
+++ task-manager/src/app/injection-tokens.ts
@@ -1,5 +1,8 @@
-import { InjectionToken } from "@angular/core";
+import { InjectionToken } from '@angular/core';
 
 export const USER_REPOSITORY_TOKEN = new InjectionToken('UserRepository');
 export const TASK_REPOSITORY_TOKEN = new InjectionToken('TaskRepository');
+export const PROJECT_REPOSITORY_TOKEN = new InjectionToken('ProjectRepository');
 export const TASK_USER_REPOSITORY_TOKEN = new InjectionToken('TaskUserRepository');
+export const PROJECT_TASK_REPOSITORY_TOKEN = new InjectionToken('ProjectTaskRepository');
+export const PROJECT_USER_REPOSITORY_TOKEN = new InjectionToken('ProjectUserRepository');
